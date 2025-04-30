4648aea228099a1b66f239e08b639a972b25d2ee
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-04-30 20:04:43 +0530

    bubble sort algo added in sorting visulizer

---
+++ helpers/sortingAlgorithms.js
@@ -0,0 +1,29 @@
+class SortingAlgorithms {
+    constructor({swapBars}){
+        this.swapBars = swapBars;
+    }
+
+    bubbleSort(array){
+        const swaps = [];
+        for (let i = 0; i < array.length; i++) {
+
+            // Last i elements are already in place
+            for (let j = 0; j < array.length -i-1; j++) {
+                // Checking if item in the present iteration is greater than next iteration
+                if(array[j]>array[j+1]){
+                    let temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                    swaps.push({ firstPosition: j, lastPosition: j+1});
+                }
+                
+            }
+            
+        }
+        return swaps;
+    }
+}
+
+export {
+    SortingAlgorithms
+}
